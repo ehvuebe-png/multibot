eb98b7a7e43a75b75ea9f3a93dfba49989cf9598
ehvuebe-png <ehvuebe@gmail.com>
2025-11-23 17:40:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,31 +1,41 @@
+const ADMIN = [8062864886]; // ID admin chính
+
+function isAdmin(id) {
+    return ADMIN.includes(id);
+}
+
 module.exports = {
-    admins: [
-        8062864886 // Admin chính
-    ],
+    admins: ADMIN,
 
     bots: [
         {
+            username: "@Concacditcu_bot",
             token: "8470961208:AAGfEuFp8YYhDevvQTrAZKO3Bci60nygGno",
             name: "BOT 1"
         },
         {
-            token: "8516563029:AAEh_n_m8dQodpIwqrxfvfO-uQbqaM6c148",
+            username: "@Concacmem_bot",
+            token: "8592926668:AAEJNY1JVcWzCi0_X4FbByh9zj6brkaPYec",
             name: "BOT 2"
         },
         {
-            token: "7525940881:AAGLlOQEE8W1WmRiXtiUlPuzwpBgcMPGA4k",
+            username: "@Anhthien_bot",
+            token: "8516563029:AAEh_n_m8dQodpIwqrxfvfO-uQbqaM6c148",
             name: "BOT 3"
         },
         {
-            token: "8282249419:AAFmsoqmiR005ODtCiFoDiQeXjFZjxyYXfU",
+            username: "@Anhthien_bot",
+            token: "8537687387:AAE4eSA-svj_JEyaR3ZEiJmbNlrrXVjvcd8",
             name: "BOT 4"
         },
         {
-            token: "8537687387:AAE4eSA-svj_JEyaR3ZEiJmbNlrrXVjvcd8",
+            username: "@Igigulc_bot",
+            token: "7525940881:AAGLlOQEE8W1WmRiXtiUlPuzwpBgcMPGA4k",
             name: "BOT 5"
         },
         {
-            token: "8592926668:AAEJNY1JVcWzCi0_X4FbByh9zj6brkaPYec",
+            username: "@Gvfyhv_bot",
+            token: "8282249419:AAFmsoqmiR005ODtCiFoDiQeXjFZjxyYXfU",
             name: "BOT 6"
         }
     ]
