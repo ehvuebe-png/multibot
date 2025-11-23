a0a77ea9f0ee08b7d0b445016a8ae84d5e80dbd0
ehvuebe-png <ehvuebe@gmail.com>
2025-11-23 17:28:00 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    admins: [
+        8062864886, // ID của bạn
+        // Thêm ID admin khác vào đây
+    ],
+
+    bots: [
+        {
+            token: "8470961208:AAGfEuFp8YYhDevvQTrAZKO3Bci60nygGno",
+            name: "BOT 1"
+        },
+        {
+            token: "8516563029:AAEh_n_m8dQodpIwqrxfvfO-uQbqaM6c148",
+            name: "BOT 2"
+        },
+        {
+            token: "7525940881:AAGLlOQEE8W1WmRiXtiUlPuzwpBgcMPGA4k",
+            name: "BOT 3"
+        },
+        {
+            token: "8282249419:AAFmsoqmiR005ODtCiFoDiQeXjFZjxyYXfU",
+            name: "BOT 4"
+        },
+        {
+            token: "8537687387:AAE4eSA-svj_JEyaR3ZEiJmbNlrrXVjvcd8",
+            name: "BOT 5"
+        },
+        {
+            token: "8592926668:AAEJNY1JVcWzCi0_X4FbByh9zj6brkaPYec",
+            name: "BOT 6"
+        }
+    ]
+};
